d0e9722a4e9aeba8fc664543d7a70ac9d4b40f78
Nela Fenclová <1160@student.itgymnazium.cz>
2025-02-25 13:36:50 +0100

    first commit

---
+++ App.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Beauty_Game;
+
+internal class App
+{
+    public void Run()
+    {
+        
+    }
+
+    public void RndCislaLidi() 
+    {
+ 
+        for (int i = 0; i < 20; i++)
+        {
+
+            i++;
+        }
+    }
+     public void random()
+    {
+        Random rnd = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(rnd.Next(0, 100));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace The_Beauty_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.Run();
+        }
+    }
+}
